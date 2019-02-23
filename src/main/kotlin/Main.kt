de8205cca7299a6021cc03f0680a007f1f12249e
DarkMentat <indraua@gmail.com>
2019-02-23 18:47:36 +0200

    Show keyboard

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,9 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
 
 
 fun main(args: Array<String>) {
@@ -17,18 +20,84 @@ class RpgMasterAssistantBot : TelegramLongPollingBot() {
     override fun getBotToken() = System.getenv("BOT_TOKEN")
     override fun getBotUsername() = System.getenv("BOT_NAME")
 
+    private val mainMenuKeyboard = arrayListOf(
+        KeyboardRow().apply {
+            add(KeyboardButton("Tarot 3 cards"))
+            add("(nothing)")
+        },
+        KeyboardRow().apply {
+            add("Ping")
+            add("(nothing)")
+        }
+    )
+
     override fun onUpdateReceived(update: Update?) {
 
         val msg = update?.message ?: return
-        val txt = update.message?.text ?: return
+        val txt = msg.text ?: return
 
-        println("ECHO: $txt")
+        println("RECV: $txt")
 
-        execute(
-            SendMessage()
-                .setChatId(msg.chatId)
-                .setText(txt)
-                .enableMarkdown(true)
-        )
+        when(txt){
+            "/keyboard" -> {
+
+                println("SEND: keyboard")
+
+                val keyboardMarkup = ReplyKeyboardMarkup().apply {
+                    keyboard = mainMenuKeyboard
+                }
+
+                execute(
+                    SendMessage()
+                        .setChatId(msg.chatId)
+                        .setText("Here is keyboard")
+                        .setReplyMarkup(keyboardMarkup)
+                )
+            }
+
+            "Ping" -> {
+                println("SEND: ping pong")
+
+                execute(
+                    SendMessage()
+                        .setChatId(msg.chatId)
+                        .setText("Pong")
+                )
+            }
+
+            "ping" -> {
+                println("SEND: ping pong")
+
+                execute(
+                    SendMessage()
+                        .setChatId(msg.chatId)
+                        .setText("pong")
+                )
+            }
+
+            "/ping" -> {
+                println("SEND: ping pong")
+
+                execute(
+                    SendMessage()
+                        .setChatId(msg.chatId)
+                        .setText("pong")
+                )
+            }
+
+            "Tarot 3 cards" -> {}
+            "(nothing)" -> {}
+
+            else -> {
+
+                println("SEND: echo \"$txt\"")
+
+                execute(
+                    SendMessage()
+                        .setChatId(msg.chatId)
+                        .setText(txt)
+                )
+            }
+        }
     }
 }
