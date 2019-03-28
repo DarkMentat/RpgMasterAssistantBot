409e1103ec92e1c40676afca48303dda715553ab
DarkMentat <indraua@gmail.com>
2019-03-28 12:57:16 +0200

    Add Motives Nature Handler

--- src/main/kotlin/NatureHandler.kt
+++ src/main/kotlin/NatureHandler.kt
@@ -18,12 +18,21 @@ class NatureHandler(
 
     override val callbackCommands = listOf("/nature_get_new")
 
-    val natures = listOf(
+    private val natures = listOf(
         "Архитектор", "Автократ", "Бон Вивант", "Браво", "Опекун", "Проповедник", "Ребенок", "Победитель", "Конформист", "Ловкач", "Брюзга",
         "Асоциал", "Директор", "Фанатик", "Щеголь", "Судья", "Одиночка", "Мученик", "Мазохист", "Монстр", "Педагог", "Кающийся грешник", "Перфекционист",
         "Бунтарь", "Плут", "Мастер выживания", "Авантюрист", "Традиционалист", "Шутник", "Мечтатель", "Идеалист", "Солдат", "Дилетант", "Естествоиспытатель",
         "Капиталист", "Хамелеон", "Калейдоскоп ужасов", "Загадка", "Глаз бури", "Гуру", "Садист", "Социопат", "Артист", "Трус", "Исследователь",
         "Фаталист", "Футурист", "Чужак", "Вопрошающий", "Варвар", "Защитник", "Новатор", "Сорвиголова"
+    )
+
+    private val motives = listOf(
+        "Получить больше власти/денег/влияния", "Создать себе имя", "Отомстить за брата/сестру/родителей/возлюбленных", "Разрушить систему",
+        "Защитить независимое место или заведение", "Получить кровь/наркотики", "Заняться сексом", "Найти способ прекратить этот кошмар",
+        "Упиваться ненужной роскошью", "Защитить свою репутацию", "Добиться справедливости для обиженных", "Дать/предложить прощение виновным",
+        "Выяснить правду о ком-то", "Отыскать идеальный элемент для устройства/проекта/ритуала", "Построить нечто прекрасное где-то",
+        "Завершить дело всей жизни", "Одарить неимущих", "Подняться по карьерной лестнице", "Отыскать способ остаться в этом городе/измерении/мире",
+        "Воссоединиться с возлюбленным", "Поделиться с миром своей болью"
     )
 
     private val inlineUpdateButton = InlineKeyboardMarkup().apply {
@@ -39,10 +48,11 @@ class NatureHandler(
 
         val nature = natures.random()
         val demeanor = natures.random()
+        val motive = motives.random()
 
         return SendMessage()
             .setChatId(update.message.chatId)
-            .setText("Натура: $nature\nМаска: $demeanor")
+            .setText("Натура: $nature\nМаска: $demeanor\nМотив: $motive")
             .setReplyMarkup(inlineUpdateButton)
     }
 
@@ -56,14 +66,14 @@ class NatureHandler(
                     .setReplyMarkup(InlineKeyboardMarkup())
             )
 
-
             val nature = natures.random()
             val demeanor = natures.random()
+            val motive = motives.random()
 
             sender.execute(
                 SendMessage()
                     .setChatId(callbackQuery.message.chatId)
-                    .setText("Натура: $nature\nМаска: $demeanor")
+                    .setText("Натура: $nature\nМаска: $demeanor\nМотив: $motive")
                     .setReplyMarkup(inlineUpdateButton)
             )
         }
